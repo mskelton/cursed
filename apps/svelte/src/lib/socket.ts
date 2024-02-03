108ada6f2b9128b47980bb2e4c2662867b4f469a
Mark Skelton <mdskelton99@gmail.com>
2024-02-03 11:27:11 -0600

    Queue and info for Svelte

--- apps/svelte/src/lib/socket.ts
+++ apps/svelte/src/lib/socket.ts
@@ -5,15 +5,25 @@ type Subscriber = (event: MessageEvent) => void
 
 const subscribers: Set<Subscriber> = new Set()
 let socket: WebSocket | null = null
+let queue: string[] = []
 let disconnected = false
 
 const name = `${faker.person.firstName()} ${faker.person.lastName()}`
 const url = `ws://${location.hostname}:8081/chat?name=${name}`
 
+function drainQueue() {
+  if (socket?.readyState !== WebSocket.OPEN) return
+  queue.forEach((message) => socket?.send(message))
+  queue = []
+}
+
 function connect() {
   socket = new WebSocket(url)
 
   socket.addEventListener("open", () => {
+    socket?.send(stringify({ type: "info" }))
+    drainQueue()
+
     if (disconnected) {
       console.log("Reconnected to server.")
       disconnected = false
@@ -35,8 +45,11 @@ function connect() {
 connect()
 
 export function send(message: ClientMessage) {
-  if (socket?.readyState !== WebSocket.OPEN) return
-  socket.send(stringify(message))
+  if (socket?.readyState === WebSocket.OPEN) {
+    socket.send(stringify(message))
+  } else {
+    queue.push(stringify(message))
+  }
 }
 
 export function subscribe(callback: Subscriber) {
